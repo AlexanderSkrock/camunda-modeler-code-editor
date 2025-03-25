849e482d5829e99f64233a0d5e32c9f13111c962
Alexander Skrock <alexanderskrock@gmx.de>
2025-03-25 11:24:42 +0100

    chore(client): removed close button

--- client/components/Modal.js
+++ client/components/Modal.js
@@ -1,18 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'camunda-modeler-plugin-helpers/react';
-
 import { Modal } from 'camunda-modeler-plugin-helpers/components';
 
 const Title = Modal.Title || (({ children }) => <h2>{children}</h2>);
 const Body = Modal.Body || (({ children }) => <div>{children}</div>);
 const Footer = Modal.Footer || (({ children }) => <div>{children}</div>);
 
-export default ({ title, children, onClose }) => (
+export default ({ title, children, onClose, footer }) => (
   <Modal onClose={ onClose }>
     <Title>{ title } </Title>
-    <Body> { children } </Body>
-    <Footer>
-      <button onClick={ onClose }>Close</button>
-    </Footer>
+    <Body>{ children } </Body>
+    { footer ? <Footer>{ footer }</Footer> : null }
   </Modal>
 );
