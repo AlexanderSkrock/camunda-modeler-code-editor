c95235d56b76decf74c77d6ca8e574c83b7938cf
Alexander Skrock <alexanderskrock@gmx.de>
2025-03-27 10:00:46 +0100

    chore(bpmn-js): removed unused property implementations

--- client/bpmn-js/utils/scripts.js
+++
@@ -1,9 +0,0 @@
-import { is } from 'bpmn-js/lib/util/ModelUtil';
-
-export function isScript(element) {
-  return is(element, 'camunda:Script');
-}
-
-export function getScriptProperty(element) {
-  return isScript(element) ? 'value' : 'script';
-}
