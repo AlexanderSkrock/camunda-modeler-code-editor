ac9cb9837b962f7369e17e130105e968d6ef5eb2
Alexander Skrock <alexanderskrock@gmx.de>
2025-03-29 14:47:14 +0100

    feat(search): added additional properties for results

--- client/utils/elements.js
+++ client/utils/elements.js
@@ -1,8 +1,19 @@
-import { getDi } from 'bpmn-js/lib/util/ModelUtil';
+import { getBusinessObject, getDi } from 'bpmn-js/lib/util/ModelUtil';
 
 export function isSameElement(elementA, elementB) {
   if (!elementA || !elementB) {
     return false;
   }
   return getDi(elementA).id === getDi(elementB).id;
+}
+
+export function getType(element) {
+  const businessObject = getBusinessObject(element);
+  if (businessObject) {
+    const descriptor = businessObject.$descriptor;
+    if (descriptor) {
+      return descriptor.name;
+    }
+  }
+  console.warn('unable to find the model descriptor');
 }
