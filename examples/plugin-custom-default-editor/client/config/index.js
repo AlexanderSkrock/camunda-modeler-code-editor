23cfc1f2bfa4910f52ca7daf751b184612b1aaed
Alexander Skrock <alexanderskrock@gmx.de>
2025-10-23 00:14:15 +0200

    feat(examples): introduced two examples for custom distributions

---
+++ examples/plugin-custom-default-editor/client/config/index.js
@@ -0,0 +1,1 @@
+export { default as applyDefaultEditor } from './applyDefaultEditor';
--- examples/plugin-custom-default-editor/client/editors/DefaultEditor.jsx
+++ examples/plugin-custom-default-editor/client/editors/DefaultEditor.jsx
@@ -3,19 +3,17 @@ import React from 'react';
 import AceEditor from 'react-ace';
 
 import 'ace-builds/src-noconflict/mode-text';
-import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-chaos';
 import 'ace-builds/src-noconflict/ext-searchbox';
 
-const DefaultEditor = ({ key, value, onValueChange }) => (
+export default ({ key, value, onValueChange }) => (
   <AceEditor
-    name={ `text-editor-${key}` }
-    mode="text"
-    theme="github"
+    name={ `default-editor-${key}` }
     width="100%"
     height="100%"
+    mode="text"
+    theme="chaos"
     defaultValue={ value }
     onChange={ onValueChange }
   />
 );
-
-export default DefaultEditor;
