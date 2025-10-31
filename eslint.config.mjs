793babd4d65c9bacbe0fa5336e4a186c2f96e995
Alexander Skrock <alexanderskrock@gmx.de>
2025-10-31 23:55:03 +0100

    chore(tests): initial setup of jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default defineConfig([ globalIgnores([ 'node_modules/*', 'dist/*' ]), {
   extends: [ 'bpmn-io/jsx', 'bpmn-io/node', 'bpmn-io/browser' ],
 
   plugins: {
     'bpmn-io': bpmnIoPlugin,
+    'jest': jestPlugin,
   },
 } ]);
